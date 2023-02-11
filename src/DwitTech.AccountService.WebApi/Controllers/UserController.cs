5130110975decf5cd15297c349dab7de33bcc11f
johnny2565 <masterj6525@gmail.com>
2023-02-11 13:51:11 +0530

    Committing all

--- src/DwitTech.AccountService.WebApi/Controllers/UserController.cs
+++ src/DwitTech.AccountService.WebApi/Controllers/UserController.cs
@@ -6,5 +6,56 @@ namespace DwitTech.AccountService.WebApi.Controllers
 {
     public class UserController : BaseController
     {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllUsers()
+        {
+            var allUsers = _userService.GetAllUsers();
+            return Ok(allUsers);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetUserById(int id) 
+        {
+            var user = _userService.GetUserById(id);
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(UserCreateDto user)
+        {
+           var createNew =  _userService.CreateUser(user);
+            return Ok(createNew);
+        }
+
+        [HttpPut("{id}", Name ="GetUserById")]
+        public ActionResult UpdateUser(int id, UserCreateDto userCreateDto)
+        {
+            var singleUser = _userService.GetUserById(id);
+            if(singleUser != null)
+            {
+                _userService.UpdateUser(userCreateDto);
+                return NoContent();
+            }
+
+            return NotFound("User Not Found");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id, UserReadDto userCreateDto) {
+            var findUser = _userService.GetUserById(id);
+            if(findUser != null)
+            {
+                _userService.DeleteUser(userCreateDto);
+                return NoContent();
+            }
+            return NotFound();
+        }
+
     }
 }
